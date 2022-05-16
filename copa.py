4b15e6ef18bdbc7766ad25d9d3e672e98b4cb145
Lucía Castro <castrolucia2108@gmail.com>
2022-05-15 23:39:18 -0300

    Agregué documentación y cambie detalles

--- copa.py
+++ copa.py
@@ -1,19 +1,22 @@
 # FUNCIONES 
 
+# Función que imprime la lista de canciones desconocidas
 def mostrarDesconocidas(listaDesconocidas):
     print()
     print(" --- CANCIONES DESCONOCIDAS --- ")
     for nombre in listaDesconocidas:
         print(f" -> {nombre}")
 
+# Función que imprime el ranking de las siete canciones más escuchadas
 def mostrarSiete(listaCanciones):
     print()
     print(" --- RANKING DE LAS 7 CANCIONES MAS ESCUCHADAS --- ")
     print()
-    for i in range(2):
+    for i in range(7):
         nombre = listaCanciones[i][0]
         print(f" -> Ranking {i + 1} : {nombre}")
 
+# Función que saca el promedio de puntos que tuvo cada canción
 def sacarPromedioCanciones(listaCanciones, cantEncuestados):
     for cancion in listaCanciones:
         
@@ -21,6 +24,7 @@ def sacarPromedioCanciones(listaCanciones, cantEncuestados):
         promedio = puntos / cantEncuestados
         cancion[3] = promedio
 
+# Función que determina que canciones no fueron nunca escuchadas
 def cancionesDesconocidas(listaCanciones):
     noEscuchadas = list()
     for cancion in listaCanciones:
@@ -33,6 +37,7 @@ def cancionesDesconocidas(listaCanciones):
     
     return noEscuchadas
 
+# Función que verifica que el puntaje que se ingresa sea entre 1 y 10
 def verificacionPuntos():
     print()
     print(" En una escala del 1 al 10 ")
@@ -46,6 +51,7 @@ def verificacionPuntos():
     
     return puntos
 
+# Función que realiza la encuesta
 def realizarEncuesta(listaCanciones):
     print()
     print(" --- REALIZAMOS LA ENCUESTA --- ")
@@ -55,18 +61,20 @@ def realizarEncuesta(listaCanciones):
         print(" -> La cancion es : ", cancion[0])
         escucho = input(" Escuchaste esta cancion ? ( si = S / no = N ) ")
         
+        # Solo si escuchó la canción puede votar su puntuación
         if escucho == "S":
             puntos = verificacionPuntos()
             cancion[3] += puntos
         
         print()
 
+# Función en la que se ingresan los datos de las canciones
 def cargarCanciones():
     print()
     print(" --- INGRESAR LOS DATOS DE LAS CANCIONES --- ")
     print()
     listaCanciones = list()
-    for i in range(4):
+    for i in range(10):
         print()
         nombre = input(" > Ingrese el nombre de la cancion : ")
         mundial = int(input(" > Ingrese el año del mundial : "))
@@ -89,13 +97,13 @@ cantEncuestados = 0
 print()
 encuesta = input(" > Desea realizar encuesta : ( si = S / no = N ) ")
 
+# Solo se va a ejecutar la función realizarEncuesta si se ingresó un si
 while encuesta != "N":
     realizarEncuesta(listaCanciones)
     cantEncuestados += 1
     print()
     encuesta = input(" > Desea realizar encuesta : ( si = S / no = N ) ")
 
-# listaCanciones = [["asdf", 1, "asdf", 4],["asdf", 1, "asdf", 6],["asdf", 1, "asdf", 2],["asdf", 1, "asdf", 3]]
 
 if cantEncuestados != 0:
     sacarPromedioCanciones(listaCanciones, cantEncuestados)
@@ -103,6 +111,10 @@ if cantEncuestados != 0:
     listaCanciones = sorted(listaCanciones, key= lambda x: x[3], reverse=True)
     mostrarSiete(listaCanciones)
     mostrarDesconocidas(listaDesconocidas)
+else:
+    print()
+    print()
+    print(" *** No se ha realizado ninguna encuesta *** ")
 
 
 
